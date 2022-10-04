1873c20e6692b6fb739669e63a53a3d1036e3439
김재성 <kjs930703@gmail.com>
2022-10-04 16:06:36 +0900

    junit maven 의존성 추가

---
+++ src/test/java/MapstructTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MapstructTest {
+
+	@Test
+	void test() {
+		Assertions.assertAll(()-> System.out.println("this is junit test for mapstruct"));
+	}
+}
